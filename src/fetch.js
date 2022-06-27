dd4d8b2ce3c8c5de0b5b0a6050a29fb39ed3a6dd
cpx2017 <56143786+cpx2017@users.noreply.github.com>
2022-06-27 09:35:58 +0700

    Update fetch.js

--- src/fetch.js
+++ src/fetch.js
@@ -6,11 +6,23 @@ ul = "https://apiweb.cpxdev.tk";
 reg = "Worldwide"
 
 
-function Flowup() {
-    return {
+async function Flowup() {
+     const response = await fetch(ul + '/home/status', {
+    method: 'GET', // *GET, POST, PUT, DELETE, etc.
+  });
+    if (response.text() == "OK") {
+        return {
         ul: ul,
         nme: reg
     }
+    } else {
+         return {
+            ul: "https://cpxapiweb.somee.com",
+            nme: reg
+        }
+    }
+        
+    
 }
 
 export default Flowup;
