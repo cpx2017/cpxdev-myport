ac4227d59d121873edf6f284a75483f5aa8ea145
cpx2017 <56143786+cpx2017@users.noreply.github.com>
2022-06-28 10:27:50 +0700

    Update fetch.js

--- src/fetch.js
+++ src/fetch.js
@@ -4,9 +4,14 @@ var reg = 'Loading';
 
 
 function Flowup() {
-     if (Math.floor(Math.random() * 2) == 0) {
+     if (Math.floor(Math.random() * 3) == 1) {
           return {
              ul: "https://cpxapiweb2.azurewebsites.net",
+             nme: "Worldwide"
+         }
+     } else if (Math.floor(Math.random() * 3) == 2) {
+          return {
+             ul: "https://apiweb.cpxdev.tk",
              nme: "Worldwide"
          }
      } else {
